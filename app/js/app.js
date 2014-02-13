74ea5d5be02479077ee46d52559671b9ecdffcf4
Edward Henn <emhenn@ucdavis.edu>
2014-02-13 13:37:31 -0800

    first commit - angular seed, simplified for a single page app

---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular
+	.module('gpaCalc', [])
+	.controller('gpaCalcController', function GpaCalcController($scope) {
+	});
