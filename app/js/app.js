694479d75e2bb6db00cc33083aec6aae7029ef8e
Edward Henn <emhenn@ucdavis.edu>
2014-02-14 12:23:56 -0800

    add course; course list repeater

--- app/js/app.js
+++ app/js/app.js
@@ -4,5 +4,24 @@
 angular
 	.module('gpaCalc', [])
 	.controller('gpaCalcController', function GpaCalcController($scope) {
+		function Course (newcourse) {
+			this.schedule = newcourse.schedule;
+			this.term = newcourse.term;
+			this.year = newcourse.year;
+			this.grade = newcourse.grade;
+			this.units = newcourse.units;
+			this.adjustedUnits = function () {
+				return this.units * (this.schedule === 'semester' ? 1 : 2/3);
+			}
+		}
 		$scope.terms = [ 'Winter', 'Spring', 'Summer', 'Fall' ];
+		$scope.addClass = function (course, courseForm) {
+			if (courseForm.$invalid) {
+				return;
+			}
+			if ($scope.courses === undefined) {
+				$scope.courses = [];
+			}
+			$scope.courses.push(new Course(course));
+		};
 	});
