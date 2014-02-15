885b59e57f4ebe295581e556f771462282efae7f
Edward Henn <emhenn@ucdavis.edu>
2014-02-15 13:09:47 -0800

    add underscore.js; adjusted unit total using underscore reduce

--- app/js/app.js
+++ app/js/app.js
@@ -4,15 +4,15 @@
 angular
 	.module('gpaCalc', [])
 	.controller('gpaCalcController', function GpaCalcController($scope) {
-		function Course (newcourse) {
+		function Course(newcourse) {
 			this.schedule = newcourse.schedule;
 			this.term = newcourse.term;
 			this.year = newcourse.year;
 			this.grade = newcourse.grade;
 			this.units = newcourse.units;
 			this.adjustedUnits = function () {
-				return this.units * (this.schedule === 'semester' ? 1 : 2/3);
-			}
+				return this.units * (this.schedule === 'semester' ? 1 : 2 / 3);
+			};
 		}
 		$scope.terms = [ 'Winter', 'Spring', 'Summer', 'Fall' ];
 		$scope.addClass = function (course, courseForm) {
@@ -26,5 +26,12 @@ angular
 		};
 		$scope.deleteClass = function (course) {
 			$scope.courses.splice($scope.courses.indexOf(course), 1);
-		}
+		};
+		$scope.adjustedUnitTotal = function () {
+			return _.reduce($scope.courses,
+				function (memo, crse) {
+					return memo + crse.adjustedUnits();
+				},
+				0);
+		};
 	});
