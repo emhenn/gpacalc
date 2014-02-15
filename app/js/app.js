88cea61ae018ff94c136416c0f111aac2d5557e7
Edward Henn <emhenn@ucdavis.edu>
2014-02-15 13:54:36 -0800

    add gradePoints function

--- app/js/app.js
+++ app/js/app.js
@@ -13,6 +13,41 @@ angular
 			this.adjustedUnits = function () {
 				return this.units * (this.schedule === 'semester' ? 1 : 2 / 3);
 			};
+			this.gradePoints = function () {
+				switch (this.grade) {
+				case "A+":
+					return 4.33;
+				case "A":
+					return 4.0;
+				case "A-":
+					return 3.67;
+				case "AB":
+					return 3.5;
+				case "B+":
+					return 3.33;
+				case "B":
+					return 3.0;
+				case "B-":
+					return 2.67;
+				case "BC":
+					return 2.5;
+				case "C+":
+					return 2.33;
+				case "C":
+					return 2.0;
+				case "C-":
+					return 1.67;
+				case "CD":
+					return 1.5;
+				case "D+":
+					return 1.33;
+				case "D":
+					return 1.0;
+				case "D-":
+					return 0.67;
+				}
+				return 0;
+			};
 		}
 		$scope.courses = [];
 		$scope.terms = [ 'Winter', 'Spring', 'Summer', 'Fall' ];
