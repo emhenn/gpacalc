9bf31b99f1191cc232491e02b7d3cbefff451c6b
Edward Henn <emhenn@ucdavis.edu>
2014-02-20 13:14:18 -0800

    first passing test - creating controller and injecting scope; termlabelfor function tested

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,16 +1,25 @@
+/*jslint indent: 4, maxerr: 50, browser: true, devel: true, nomen: true, plusplus: true */
 'use strict';
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('gpaCalc'));
+describe('gpaCalcController', function () {
+	var scope, $controllerConstructor;
+
+	beforeEach(module('gpaCalc'));
+
+	beforeEach(inject(function ($controller, $rootScope) {
+		scope = $rootScope.$new();
+		$controllerConstructor = $controller;
+	}));
 
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+	it('should return correct term labels', inject(function () {
+		var ctrl = $controllerConstructor('gpaCalcController', { $scope: scope });
+		expect(scope.termLabelFor(1)).toBe('Winter');
+		expect(scope.termLabelFor(2)).toBe('Spring');
+		expect(scope.termLabelFor(3)).toBe('Summer');
+		expect(scope.termLabelFor(4)).toBe('Fall');
+	}));
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
 });
