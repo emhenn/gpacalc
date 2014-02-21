4d9aed1b05c6e6a91493074d35173642addd251a
Edward Henn <emhenn@ucdavis.edu>
2014-02-21 10:04:48 -0800

    make grade values and term unit multiplier core objects

--- app/js/core.js
+++ app/js/core.js
@@ -3,7 +3,25 @@ var gpaCore = (function () {
 
 	return {
 		termLabels: { 1: 'Winter', 2: 'Spring', 3: 'Summer', 4: 'Fall' },
-
+		termMultiplier: { 'semester': 1, 'quarter': 2 / 3 },
+		gradeValues: {
+			'A+': 4.33,
+			'A':  4.0,
+			'A-': 3.67,
+			'AB': 3.5,
+			'B+': 3.33,
+			'B':  3.0,
+			'B-': 2.67,
+			'BC': 2.5,
+			'C+': 2.33,
+			'C':  2.0,
+			'C-': 1.67,
+			'CD': 1.5,
+			'D+': 1.33,
+			'D':  1.0,
+			'D-': 0.67,
+			'F':  0
+		},
 		Course: function (newcourse) {
 			this.schedule = newcourse.schedule;
 			this.term = newcourse.term;
@@ -15,42 +33,10 @@ var gpaCore = (function () {
 				return gpaCore.termLabels[this.term];
 			};
 			this.adjustedUnits = function () {
-				return this.units * (this.schedule === 'semester' ? 1 : 2 / 3);
+				return this.units * gpaCore.termMultiplier[this.schedule];
 			};
 			this.gradeValue = function () {
-				switch (this.grade) {
-				case "A+":
-					return 4.33;
-				case "A":
-					return 4.0;
-				case "A-":
-					return 3.67;
-				case "AB":
-					return 3.5;
-				case "B+":
-					return 3.33;
-				case "B":
-					return 3.0;
-				case "B-":
-					return 2.67;
-				case "BC":
-					return 2.5;
-				case "C+":
-					return 2.33;
-				case "C":
-					return 2.0;
-				case "C-":
-					return 1.67;
-				case "CD":
-					return 1.5;
-				case "D+":
-					return 1.33;
-				case "D":
-					return 1.0;
-				case "D-":
-					return 0.67;
-				}
-				return 0;
+				return gpaCore.gradeValues[this.grade] || 0;
 			};
 			this.gradePoints = function () {
 				return this.gradeValue() * this.adjustedUnits();
