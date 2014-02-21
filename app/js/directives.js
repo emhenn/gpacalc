9cb8ef1d9fed40dae15ca421908c0b7e19709f8e
Edward Henn <emhenn@ucdavis.edu>
2014-02-21 09:44:31 -0800

    remove debug statement from directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,7 +4,6 @@ gpaCalcApp.directive('focusMe', function () {
 	return {
 		link: function (scope, element, attrs) {
 			scope.$watch('focusMe.' + attrs.id, function (value) {
-				console.log('value=', value);
 				if (value === true) {
 					element[0].focus();
 					scope.focusMe[attrs.id] = false;
